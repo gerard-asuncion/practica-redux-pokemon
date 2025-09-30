b72c836dc980d9b8866ba9f880901693eedb537b
GerardAsuncion <asunciongerard90@gmail.com>
2025-09-30 20:15:57 +0200

    fix: removed unused import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
 import './index.css'
 import App from './App.jsx'
-import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 import { pokemonsReducer } from './reducers/pokemons.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
