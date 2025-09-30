9522621c0072ec82e7dc402365202f26354d7d05
GerardAsuncion <asunciongerard90@gmail.com>
2025-09-30 13:08:10 +0200

    feat: created PokemonCard component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,16 @@
+import { Card } from "antd";
+import Meta from "antd/es/card/Meta";
+
+const PokemonCard = () => {
+  return (
+    <Card
+        style={{ width: 250}}
+        title="Ditto"
+        cover={<img src="" alt="Ditto" />}
+    >
+        <Meta description="fire, magic" />
+    </Card>
+  )
+}
+
+export default PokemonCard
