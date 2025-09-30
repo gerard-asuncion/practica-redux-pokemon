53627379136fed874ead9c2c7926b7ba63ca1e3c
GerardAsuncion <asunciongerard90@gmail.com>
2025-09-30 18:47:47 +0200

    feat: starting to implement redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { pokemonsReducer } from './reducers/pokemons.js'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
+
+const store = createStore(pokemonsReducer);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
