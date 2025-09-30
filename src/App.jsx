71b182af3a0e3a398f04425aa595411f639c8479
GerardAsuncion <asunciongerard90@gmail.com>
2025-09-30 12:59:09 +0200

    feat: created Searcher component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { Col } from 'antd'
+import Searcher from './components/Searcher'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+    </div>
   )
 }
 
