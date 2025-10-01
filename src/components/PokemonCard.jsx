73166ea2b501e1879a57647c838e5ea579e6b8bb
GerardAsuncion <asunciongerard90@gmail.com>
2025-10-01 19:06:15 +0200

    feat: implemented favorite pokemon feature

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,6 +2,8 @@ import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
 import StarButton from "./StarButton";
 import "./PokemonList.css";
+import { useDispatch } from "react-redux";
+import { setFavorite } from "../actions";
 
 const PokemonCard = ({
 	name,
@@ -10,16 +12,27 @@ const PokemonCard = ({
 	id,
 	favorite,
 }) => {
+  const dispatch = useDispatch();
+
+  const handleOnFavorite = () => {
+    dispatch(setFavorite({ pokemonId: id }));
+  }
+
   return (
     <Card
         id={id}
 			  title={name}
 			  cover={image && <img src={image.front_default} alt={name} />}
         extra={
-				<StarButton isFavorite={favorite ?? false} />
+				<StarButton isFavorite={favorite ?? false} onClick={handleOnFavorite} />
 			}
     >
-        <Meta description="fire, magic" />
+      <Meta description="Types:" />
+			<ul style={{color: 'gray'}}>
+				{types?.map((type) => (
+					<li key={type.type.name}>{type.type.name}</li>
+				))}
+			</ul>
     </Card>
   )
 }
