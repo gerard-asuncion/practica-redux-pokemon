594eed04a790382f2ca67f5d417f5b64c0d80c08
GerardAsuncion <asunciongerard90@gmail.com>
2025-10-01 19:50:04 +0200

    feat: starting to implement redux toolkit, dataSlice created, rootReducer modified, page is not displaying now

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import { pokemonsReducer } from "./pokemons";
-import { uiReducer } from "./ui";
+import dataReducer from "../slices/dataSlice"
 
 
 const rootReducer = combineReducers({
-    data: pokemonsReducer,
-    ui: uiReducer,
+    data: dataReducer,
 });
 
 export default rootReducer
